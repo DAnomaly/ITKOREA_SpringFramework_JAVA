899e7313b2edfe0774d8a605219d29d65c67695c
DAnomaly <parksh1219@naver.com>
2021-04-07 17:45:34 +0900

    [210407]UPDATE:자바학습총정리.md]

--- workplace-0304/07_inheritance/src/ex03_has_a/MainClass.java
+++ workplace-0304/07_inheritance/src/ex03_has_a/MainClass.java
@@ -1,7 +1,7 @@
 package ex03_has_a;
 // has -a 관계로 상속으로 처리할 수 있습니다.
 // Cirele has a cored 원하는 점을 가지고 한다포함한다.
-// circle클래스르르 서브클래스로 지정합니다.
+// Cirele클래스를 서브클래스로 지정합니다.
 
 public class MainClass {
 
